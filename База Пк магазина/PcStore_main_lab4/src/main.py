5e196b8395de23025ec1eac625249838f4c13f17
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-11-02 19:22:15 +0300

    Delete База Пк магазина/PcStore_main_lab4/src directory

--- База Пк магазина/PcStore_main_lab4/src/main.py
+++
@@ -1,7 +0,0 @@
-from sql import check_base, create_base
-BASE_PATH = 'PcStore.db'
-def check_create_base():
-    if not check_base(BASE_PATH):
-        create_base(BASE_PATH, 'sql/base.sql')
-if __name__ == '__main__':
-    check_create_base()
