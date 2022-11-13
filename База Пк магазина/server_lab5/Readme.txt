21f37855191acec170fac499b2d88c2492f0d563
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-11-13 20:11:08 +0300

    Delete База Пк магазина/server_lab5 directory

--- База Пк магазина/server_lab5/Readme.txt
+++
@@ -1,6 +0,0 @@
-pip install fastapi
-pip install uvicorn
-
-**
-api.docx - в будущем сделаю
-routers - не доделан, для примера пока только users
--- База Пк магазина/server_lab5/src/resolvers/manager.py
+++
@@ -1,9 +0,0 @@
-from sql_base import BaseWorker
-from sql_base import models
-
-def new_manager(manager: models.manager) -> int:
-    new_id = BaseWorker.insert_data("INSERT INTO manager(user_id) "
-                                     "VALUES(?) "
-                                     "RETURNING id",
-                                     (manager.user_id))
-    return new_id
