2f75c18e4d2560b8c5849ba61115da4fcdde3940
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-11-27 12:13:18 +0300

    Update check_login.py

--- База Пк магазина/server_GUI_lab6/src/resolvers/check_login.py
+++ База Пк магазина/server_GUI_lab6/src/resolvers/check_login.py
@@ -2,5 +2,5 @@ from sql_base.models import usersM
 from sql_base.base import base_worker
 
 def check_login_request(user: usersM):
-    post_id = base_worker.insert_data(f"SELECT id FROM user WHERE username = '{user.username}' AND password = '{user.password}'",())
+    post_id = base_worker.insert_data(f"SELECT id FROM user WHERE username = ? AND password = ?",(user.username,user.password))
     return post_id
