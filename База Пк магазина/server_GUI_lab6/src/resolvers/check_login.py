e9ec05656ff91e5b44c53e7781a29231ee38432e
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-12-22 21:29:44 +0300

    Delete База Пк магазина/server_GUI_lab6 directory

--- База Пк магазина/server_GUI_lab6/src/resolvers/check_login.py
+++
@@ -1,6 +0,0 @@
-from sql_base.models import usersM
-from sql_base.base import base_worker
-
-def check_login_request(user: usersM):
-    post_id = base_worker.insert_data(f"SELECT id FROM user WHERE username = ? AND password = ?",(user.username,user.password))
-    return post_id
