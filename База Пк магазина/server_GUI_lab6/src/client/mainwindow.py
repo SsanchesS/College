366c3fc46f0fb41612b2f23ac2103d51fd00f28d
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-12-22 20:08:06 +0300

    Add files via upload

--- База Пк магазина/server_GUI_lab6/src/client/mainwindow.py
+++ База Пк магазина/server_GUI_lab6/src/client/mainwindow.py
@@ -1,31 +1,49 @@
-import tkinter as tk
+from tkinter import *
 import tkinter.messagebox as mesbox
-from login_form import LoginForm
+from  my_api import *
 
-font = ('Arial Bold', 30)
+font = ('Arial Bold', 16)
 
-class MainWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.btn = tk.Button(text="Login", font=font,
-                             command=self.open_login)
-        self.btn.pack(pady=20, padx=50)
-        self.open_login()
+root = Tk()
+root.title("My GUI")  
+root.geometry('800x600')
 
-    def open_login(self):
-        login_form = LoginForm(self)
-        post = login_form.open()
-        if post:
-            print("Login ok")
-        else:
-            mesbox.showerror(title="Wrong login",
-                                          message="Логин или пароль не верны"
-                                          )
-            self.open_login()
+username = StringVar()
+userpassword = StringVar()
 
+
+def open_login():
+    post = check_login(username=username.get(),password=userpassword.get())
+    if post:
+        print("Login ok")
+        print(post)
+        root.withdraw()
+        create_app(root,font)
+    else:
+        mesbox.showerror(title="Wrong login",message="Логин или пароль не верны")
+
+
+
+
+lbl_main = Label(root, text="Вход в систему", font=font)
+lbl_login = Label(root, text="Логин", font=font)
+lbl_pass = Label(root, text='Пароль', font=font)
+
+entry_login = Entry(root, font=font, textvariable=username)
+entry_pass = Entry(root, font=font,show="*", textvariable=userpassword)
+
+btn_enter = Button(root, text='Вход', font=font, command=open_login)
+btn_close = Button(root, text='Отмена', font=font)
+
+lbl_main.grid(row=0, columnspan=2, column=1)
+lbl_login.grid(row=1, column=0, pady=10, ipadx=10)
+lbl_pass.grid(row=2, column=0, pady=10, ipadx=10)
+
+entry_login.grid(row=1, column=1, columnspan=3, padx=30, pady=10)
+entry_pass.grid(row=2, column=1, columnspan=3, padx=30, pady=10)
+
+btn_enter.grid(row=3, column=1, pady=10)
+btn_close.grid(row=3, column=2, pady=10)
 
 if __name__ == '__main__':
-    root = MainWindow()
-    root.title("My GUI")  
-    root.geometry('800x600')
     root.mainloop()
